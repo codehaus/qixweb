38ea41ca740418aded8103d974fd9146a13236bb
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2005-07-14 08:01:31 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0002'.

---
+++ tags/BL-0002/WebAppSample/WEB-INF/sources/org/qixweb/sample/node/HelloNode.java
@@ -0,0 +1,54 @@
+package org.qixweb.sample.node;
+
+import java.util.Calendar;
+
+import org.apache.commons.lang.StringUtils;
+import org.qixweb.core.*;
+import org.qixweb.sample.command.SetGuestCommand;
+import org.qixweb.time.*;
+
+
+
+public class HelloNode extends WebNode
+{
+    private String itsName;
+    private QixwebEnvironment itsEnvironment;
+
+    public static WebNode create(WebAppUrl anUrl, UserData aUserData, QixwebEnvironment environment)
+    {
+        String guestName = anUrl.getParameter(parameterNameForGuestName());
+        return new HelloNode(environment, StringUtils.isEmpty(guestName) ? "unknown" : guestName);
+    }
+
+    public String currentTime()
+    {
+        return DateFormatter.formatDD_MM_YYYY_HH_mm_ss(new QixwebTime(Calendar.getInstance()));
+    }
+    public WebAppUrl setGuestNameUrl()
+    {
+        return new QixwebUrlFactory(itsEnvironment).createUrlWith(SetGuestCommand.class);
+    }
+    
+    public static WebAppUrl urlToMe(QixwebEnvironment environment, String guestName)
+    {
+        WebAppUrl url = new QixwebUrlFactory(environment).createUrlWith(HelloNode.class);
+        url.setParameter(parameterNameForGuestName(), guestName);
+        return url;
+    }
+    
+    public HelloNode(QixwebEnvironment environment, String name)
+    {
+        itsEnvironment = environment;
+        itsName = name;
+    }
+    
+    public static String parameterNameForGuestName()
+    {
+        return "guestName";
+    }
+    
+    public String guestName()
+    {
+        return itsName;
+    }
+}
