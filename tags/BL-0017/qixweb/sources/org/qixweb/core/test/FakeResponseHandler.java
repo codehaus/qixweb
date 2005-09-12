e14c139784c5de6049809321d10b855380ebf118
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2005-09-12 08:39:22 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0017'.

---
+++ tags/BL-0017/qixweb/sources/org/qixweb/core/test/FakeResponseHandler.java
@@ -0,0 +1,69 @@
+package org.qixweb.core.test;
+
+import java.io.IOException;
+
+import org.qixweb.core.*;
+
+
+public class FakeResponseHandler implements ResponseHandler
+{
+	private boolean throwExceptionDisplayingNode;
+	private String itsFailureNodeInTemplateMerging;
+	protected WebAppUrl itsDestinationUrl;
+	private WebNode itsNodeToDisplay;
+    private Browsable itsLastBrowsable;
+
+	public FakeResponseHandler()
+	{
+		itsFailureNodeInTemplateMerging = "";
+		throwExceptionDisplayingNode = false;
+	}
+
+	public void display(WebNode aDestinationNode) throws IOException
+	{
+		if (itsFailureNodeInTemplateMerging.equals(aDestinationNode.getClass().getName()))
+			throw new RuntimeException("Simulated FailureInTemplateMerging");
+		else if (throwExceptionDisplayingNode)
+		{
+			throwExceptionDisplayingNode = false;
+			throw new IOException("Simulated IOException");
+		}
+		
+		itsLastBrowsable = itsNodeToDisplay = aDestinationNode;
+	}
+	public void simulateExceptionSendingHTMLOnlyOnce()
+	{
+		throwExceptionDisplayingNode = true;
+	}
+
+	public void redirectTo(WebAppUrl aDestinationUrl) throws IOException
+	{
+        itsLastBrowsable = itsDestinationUrl = aDestinationUrl;
+	}
+
+	public WebAppUrl redirectedDestination()
+	{
+		return itsDestinationUrl;
+	}
+
+	public WebNode displayedNode()
+	{
+		return itsNodeToDisplay;
+	}
+
+	public void sendHtml(String aHtmlToSend)
+	{
+		throw new IllegalAccessError("Not implemented");
+	}
+
+	public void simulateFailureInTemplateMergingFor(Class anXpTrackingNode)
+	{
+		itsFailureNodeInTemplateMerging = anXpTrackingNode.getName();
+	}
+
+    public Browsable lastBrowsed()
+    {
+        return itsLastBrowsable;
+    }
+
+}
