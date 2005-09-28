48b3d0290d1f5f31767c0b7a39165055d1cdcfe5
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2005-09-28 09:55:19 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0024'.

---
+++ tags/BL-0024/qixweb/sources/org/qixweb/core/test/AnyRefreshableCommand.java
@@ -0,0 +1,24 @@
+package org.qixweb.core.test;
+
+import org.qixweb.core.*;
+import org.qixweb.util.DeepEquals;
+
+
+public class AnyRefreshableCommand implements WebCommand
+{
+	public static AnyRefreshableCommand create(WebAppUrl webUrl, UserData userData)
+	{
+		return new AnyRefreshableCommand();
+	}
+	
+	public Browsable execute(QixwebEnvironment environment)
+	{
+		return new AnyNode();
+	}
+	
+	public boolean equals(Object obj)
+	{
+		return DeepEquals.equals(this, obj);
+	}
+
+}
