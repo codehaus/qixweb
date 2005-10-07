bd5e2c39980697659551fc499723eecb2b950ec3
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2005-10-07 13:34:14 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0040'.

---
+++ tags/BL-0040/qixweb/sources/org/qixweb/core/test/TestQixwebUrlFactory.java
@@ -0,0 +1,39 @@
+package org.qixweb.core.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.qixweb.core.QixwebUrlFactory;
+import org.qixweb.core.QixwebUrl;
+
+import junit.framework.TestCase;
+
+
+
+public class TestQixwebUrlFactory extends TestCase
+{
+
+    private FakeEnvironment env;
+    private QixwebUrlFactory urlFactory;
+
+    protected void setUp() throws Exception
+    {
+        env = new FakeEnvironment();
+        urlFactory = new QixwebUrlFactory(env);
+    }
+    
+    public void testCreateFromMap() throws Exception
+    {
+        Map map = new HashMap();
+        map.put(QixwebUrl.PARAMETER_NODE_TO_DISPLAY, new String[] { "AnyNode" });
+
+        assertEquals(QixwebUrl.createAsRequestFrom(map, env.nodePackage(), env.commandPackage()), urlFactory.createFrom(map));
+    }
+    
+    public void testCreateUrlWithClass() throws Exception
+    {
+        assertEquals(new QixwebUrl(AnyCommand.class), urlFactory.createUrlWith(AnyCommand.class));
+    }
+
+
+}
