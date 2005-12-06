465fc7fb877c60149ee0ec8b0b9742c4557c207b
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2005-12-06 16:38:45 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0066'.

---
+++ tags/BL-0066/qixweb/sources/org/qixweb/time/RealTimeProvider.java
@@ -0,0 +1,14 @@
+package org.qixweb.time;
+
+import java.util.Calendar;
+
+
+public class RealTimeProvider implements TimeProvider
+{
+	public QixwebTime now()
+	{
+		return new QixwebTime(Calendar.getInstance());
+	}
+	
+	
+}
