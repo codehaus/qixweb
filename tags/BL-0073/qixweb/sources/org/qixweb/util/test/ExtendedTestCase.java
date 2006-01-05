3785b1ac309e30e00e54e10bf85c141128bda52e
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-01-05 16:07:18 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0073'.

---
+++ tags/BL-0073/qixweb/sources/org/qixweb/util/test/ExtendedTestCase.java
@@ -0,0 +1,178 @@
+package org.qixweb.util.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.lang.StringUtils;
+import org.qixweb.util.StringUtil;
+import org.qixweb.util.XpLogger;
+
+
+public abstract class ExtendedTestCase extends TestCase
+{
+	private PrintStream systemErr;
+    private PrintStream systemOut;
+    private ByteArrayOutputStream itsGrabbedErr;
+    private ByteArrayOutputStream itsGrabbedOut;
+
+    public ExtendedTestCase()
+	{
+	    super();
+        init();
+	}
+
+    public ExtendedTestCase(String aString)
+	{
+	    super(aString);
+        init();
+	}
+    
+	public static void assertDoubleEquals(String aMessage, double anExpectedDouble, double anActualDouble)
+	{
+		assertEquals(aMessage, anExpectedDouble, anActualDouble, 0.0);
+	}
+
+	public static void assertDoubleEquals(double anExpectedDouble, double anActualDouble)
+	{
+		assertEquals("", anExpectedDouble, anActualDouble, 0.0);
+	}
+	
+	public static void assertNotEquals(String aMessage, Object aNotExpectedObject, Object anObjectToVerify)
+	{
+		assertTrue(aMessage, !aNotExpectedObject.equals(anObjectToVerify));
+	}
+	
+	public static void assertNotEquals(Object aNotExpectedObject, Object anObjectToVerify)
+	{
+		assertTrue(!aNotExpectedObject.equals(anObjectToVerify));
+	}	
+
+	public static void assertFalse(String aMessage, boolean condition)
+	{
+		assertTrue(aMessage, !condition);
+	}
+
+	public static void assert_contains(String aMessage, String aString, String aSubstring)
+	{
+		assertTrue(aMessage +": '" + aString + "' should contains '" + aSubstring + "'", StringUtils.contains(aString, aSubstring));
+	}
+    
+    public static void assertEqualsIgnoringCase(String aMessage, String expected, String actual)
+    {
+        assertTrue(aMessage +": '" + expected + "' should be equals (ignoring case) to '" + actual + "'", StringUtils.equalsIgnoreCase(expected, actual));
+    }
+    
+    public static void assertEqualsIgnoringCase(String expected, String actual)
+    {
+        assertEqualsIgnoringCase("", expected, actual);
+    }    
+    
+	public static void assert_matchesRegex(String aMessage, String aString, String aRegex)
+	{
+		assertTrue(aMessage +": '" + aString + "' should contains regex '" + aRegex +"'", StringUtil.string_containsRegex(aString, aRegex));
+	}
+
+	public static void assert_matchesRegex(String aString, String aRegex)
+	{
+		assert_matchesRegex("", aString, aRegex);
+	}
+
+	public static void assert_contains(String aString, String aSubstring)
+	{
+		assert_contains("", aString, aSubstring);
+	}
+
+	public static void assert_notContains(String aMessage, String aString, String aSubstring)
+	{
+		assertFalse(aMessage, StringUtils.contains(aString, aSubstring));
+	}
+
+	public static void assert_notContains(String aString, String aSubstring)
+	{
+		assert_notContains("", aString, aSubstring);
+	}
+
+	public static void assertEmpty(Object[] someObjects)
+	{
+		assertTrue("Length is "+someObjects.length+" instead of 0", someObjects.length == 0);
+	}
+	
+	public static void assertEmpty(String aMessagge, Object[] someObjects)
+	{
+		assertTrue(aMessagge+": length is "+someObjects.length+" instead of 0", someObjects.length == 0);
+	}
+
+    protected void setUp() throws Exception
+    {
+        init();
+    }
+
+    private void init()
+    {
+        systemOut = System.out;
+        systemErr = System.err;
+    }
+    protected void tearDown() throws Exception
+    {
+        System.setOut(systemOut);
+        XpLogger.resetConsoleAppenderLogger();
+        System.setErr(systemErr);
+    }
+
+    public String grabbedErr()
+    {
+        return itsGrabbedErr.toString();
+    }
+
+    public void grabSystemErr()
+    {
+        itsGrabbedErr = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(itsGrabbedErr));
+    }
+
+    public String grabbedOut()
+    {
+        return itsGrabbedOut.toString();
+    }
+
+    public void grabSystemOutResettingLogger()
+    {
+        grabSystemOut();
+        XpLogger.resetConsoleAppenderLogger();
+    }
+
+    public void grabSystemOut()
+    {
+        itsGrabbedOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(itsGrabbedOut));
+    }
+
+    public void assertEqualsOnlyIgnoringOrder(List firstList, List secondList)
+    {
+        assertNotEquals("Lists should not be equals considering order", firstList, secondList);
+        assertEqualsIgnoringOrder("Normalizing order the lists should be equal", firstList, secondList); 
+    }
+    
+    public void assertEqualsIgnoringOrder(String message, List firstList, List secondList)
+    {
+        assertEquals(message, new HashSet(firstList), new HashSet(secondList)); 
+    }
+
+    public void assertEqualsIgnoringOrder(List firstList, List secondList)
+    {
+        assertEqualsIgnoringOrder("", firstList, secondList); 
+    }
+    
+    public void assertEquals(String message, Object[] someObjects, Object[] otherObjects)
+    {
+        if (someObjects.length != otherObjects.length)
+            fail(message + " - lenghts are different");
+
+        for (int i = 0; i < someObjects.length; i++)
+            assertEquals(message, someObjects[i], otherObjects[i]);
+    }
+}
