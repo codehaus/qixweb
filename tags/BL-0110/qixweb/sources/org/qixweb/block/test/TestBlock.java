ef76f692b6b62dab31c022f18113fa64af8da4b9
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-04-18 12:46:36 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0110'.

---
+++ tags/BL-0110/qixweb/sources/org/qixweb/block/test/TestBlock.java
@@ -0,0 +1,32 @@
+package org.qixweb.block.test;
+
+import junit.framework.TestCase;
+
+import org.qixweb.block.Block;
+
+
+public class TestBlock extends TestCase
+{
+
+    private Block itsBlock;
+    protected void setUp()
+    {
+        itsBlock = new Block();
+    }
+    
+	public void testDefaultEval()
+	{
+		Object each = new Object();
+		Object runningValue = new Object();
+		assertNull(itsBlock.eval(runningValue, each));
+        assertNull(itsBlock.eval(each));
+	}
+	public void testDefaultIs()
+	{
+		assertTrue(!itsBlock.is(new Object()));
+	}
+	public void testDefaultRun()
+	{
+		itsBlock.run(new Object());
+	}
+}
