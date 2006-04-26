34e42aea43f50733193ad6b94e4fd3a7009822e1
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-04-26 15:33:32 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0112'.

---
+++ tags/BL-0112/qixweb/sources/org/qixweb/core/Parameters.java
@@ -0,0 +1,300 @@
+package org.qixweb.core;
+
+import java.util.*;
+
+import org.apache.commons.lang.StringUtils;
+import org.qixweb.block.*;
+import org.qixweb.time.*;
+import org.qixweb.util.UrlParametersExtractor;
+
+/**
+ *
+ */
+public class Parameters
+{
+    protected Map itsParameters;
+
+    public Parameters()
+    {
+        this(new HashMap());
+    }
+
+    public Parameters(Map parameters)
+    {
+        itsParameters = parameters;
+    }
+
+    public String get(String key)
+    {
+        Object value = itsParameters.get(key);
+        if (value == null)
+            return null;
+        else if (value.getClass().isArray())
+            return ((String[]) value)[0];
+        else
+            return (String) value;
+    }
+    
+    public String get(String key, String defaultValue)
+    {
+        String result = get(key);
+        if (result == null)
+           result = defaultValue;
+        
+        return result;
+    }    
+
+    public Parameters set(String key, String value)
+    {
+        itsParameters.put(key, value);
+        return this;
+    }
+
+    public boolean getAsBoolean(String key)
+    {
+        return new Boolean(get(key)).booleanValue();
+    }
+
+    public Parameters set(String key, boolean value)
+    {
+        itsParameters.put(key, Boolean.toString(value));
+        return this;
+    }
+
+    public QixwebCalendar getAsCalendarDD_MM_YYYY(String key)
+    {
+        String date = get(key);
+        if (StringUtils.isEmpty(date))
+            return QixwebCalendar.NULL;
+        else
+            return DateFormatter.parseDDslashMMslashYYYYasQixwebDate(date);
+    }
+
+    public QixwebCalendar getAsDateWithPrefix(String keyPrefix)
+    {
+        return QixwebDate.createFrom(this, keyPrefix);
+    }
+
+    public int getAsInt(String key)
+    {
+        return Integer.parseInt(get(key));
+    }
+
+    public Parameters set(String key, int value)
+    {
+        itsParameters.put(key, Integer.toString(value));
+        return this;
+    }
+
+    public int getAsInt(String key, int defaultValue)
+    {
+        try
+        {
+            return getAsInt(key);
+        }
+        catch (Exception e)
+        {
+            return defaultValue;
+        }
+    }
+
+    public Integer getAsInteger(String key)
+    {
+        String value = get(key);
+        if (value != null)
+            return Integer.decode(value);
+        else
+            return null;
+    }
+
+    /**
+     * @deprecated
+     * @see #set(String key, String value)
+     */
+    public Parameters set(String key, Object value)
+    {
+        itsParameters.put(key, value.toString());
+        return this;
+    }
+
+    public Integer getAsInteger(String key, Integer defaultValue)
+    {
+        try
+        {
+            Integer foundValue = getAsInteger(key);
+            if (foundValue != null)
+                return foundValue;
+        }
+        catch (Exception ex)
+        {
+        }
+        return defaultValue;
+    }
+
+    public String[] getAllValuesOf(String parameterKey)
+    {
+        if (itsParameters.get(parameterKey) != null)
+        {
+            if (itsParameters.get(parameterKey).getClass().isArray())
+                return (String[]) itsParameters.get(parameterKey);
+            else
+                return new String[] { (String) itsParameters.get(parameterKey) };
+        }
+        else
+            return new String[0];
+    }
+
+    public Integer[] getAllValuesAsIntegerOf(String key)
+    {
+        String[] values = getAllValuesOf(key);
+        return (Integer[]) LightInternalIterator.createOn(values).collect(new Function()
+        {
+            public Object eval(Object eachValue)
+            {
+                try
+                {
+                    return Integer.decode((String) eachValue);
+                }
+                catch (NumberFormatException ex)
+                {
+                    return null;
+                }
+            }
+        }, Integer.class);
+    }
+
+    public Map allBeginningWith(String aPrefix)
+    {
+        HashMap parametersBeginningWithPrefix = new HashMap();
+
+        Iterator parametersIterator = itsParameters.keySet().iterator();
+        while (parametersIterator.hasNext())
+        {
+            String key = (String) parametersIterator.next();
+            if (key.startsWith(aPrefix))
+                parametersBeginningWithPrefix.put(removePrefixFrom(aPrefix, key), get(key));
+        }
+
+        return parametersBeginningWithPrefix;
+    }
+
+    public Parameters set(String key, double value)
+    {
+        itsParameters.put(key, Double.toString(value));
+        return this;
+    }
+
+    public Parameters set(String key, long value)
+    {
+        itsParameters.put(key, Long.toString(value));
+        return this;
+    }
+
+    public Parameters set(String key, String[] someValues)
+    {
+        itsParameters.put(key, someValues);
+        return this;
+    }
+
+    public Parameters set(Map newParametersMap)
+    {
+        if (newParametersMap != null)
+            itsParameters.putAll(newParametersMap);
+        return this;
+    }
+
+    public int size()
+    {
+        return itsParameters.size();
+    }
+
+    protected String removePrefixFrom(String aPrefix, String aString)
+    {
+        return aString.substring(aPrefix.length());
+    }
+
+    protected void removeAll()
+    {
+        itsParameters.clear();
+    }
+
+    public String allAsString()
+    {
+        if (itsParameters.size() > 0)
+        {
+            StringBuffer buf = new StringBuffer();
+            Object[] keys = itsParameters.keySet().toArray();
+
+            Arrays.sort(keys);
+            for (int i = 0; i < keys.length; i++)
+            {
+                String key = (String) keys[i];
+                if (itsParameters.get(key) instanceof String)
+                    append(buf, key, get(key));
+                else
+                    append(buf, key, getAllValuesOf(key));
+            }
+            buf.setCharAt(0, UrlParametersExtractor.QUESTION_MARK.charAt(0));
+
+            return buf.toString();
+        }
+        else
+            return "";
+    }
+
+    private void append(StringBuffer buf, String key, String parameterValue)
+    {
+        buf.append(UrlParametersExtractor.AMPERSAND);
+        buf.append(key);
+        buf.append(UrlParametersExtractor.EQUAL);
+        buf.append(WebUrl.encode(parameterValue));
+    }
+
+    private void append(StringBuffer buf, String key, String[] parameterValues)
+    {
+        for (int j = 0; j < parameterValues.length; j++)
+            append(buf, key, parameterValues[j]);
+    }
+
+    public Set keys()
+    {
+        return itsParameters.keySet();
+    }
+
+    public boolean equals(Object anotherObject)
+    {
+        if (anotherObject instanceof Parameters)
+        {
+            Parameters other = (Parameters) anotherObject;
+            return allAsString().equals(other.allAsString());
+        }
+        else
+            return false;
+    }
+
+    public void addExcluding(final Parameters source, final Set keysToExclude)
+    {
+      LightInternalIterator.createOn(source.itsParameters.keySet()).forEach(new Procedure()
+      {
+          public void run(Object aEach)
+          {
+              String key = (String) aEach;
+              if (! keysToExclude.contains(key))
+              {
+                  Object object = source.itsParameters.get(key);
+                  itsParameters.put(key, object);
+              }
+          }
+      });
+    }
+    
+    public void add(Parameters source)
+    {
+        addExcluding(source, new HashSet());
+    }
+    
+    public void remove(String key)
+    {
+        itsParameters.remove(key);
+    }
+}
