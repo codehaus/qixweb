c4fa6a3c881719ef26465b5b2c1ccf368a82c67e
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-06-06 10:04:56 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0124'.

---
+++ tags/BL-0124/qixweb/sources/org/qixweb/block/IsReturnValueOf_containedIn.java
@@ -0,0 +1,28 @@
+package org.qixweb.block;
+
+
+
+public class IsReturnValueOf_containedIn implements Predicate
+{
+	private String itsMethodToCall;
+	private Object[] itsValues;
+
+	public IsReturnValueOf_containedIn(String methodToCall, Object[] someValues)
+	{
+		itsValues = someValues;
+		itsMethodToCall = methodToCall;
+	}
+
+	public boolean is(Object each)
+	{
+		CallGetter callGetter = new CallGetter(itsMethodToCall);
+		Object valueToCompare = callGetter.eval(each);	
+
+		for (int i = 0; i < itsValues.length; i++)
+		{
+			if (valueToCompare.equals(itsValues[i]))
+				return true;
+		}
+		return false;
+	}
+}
