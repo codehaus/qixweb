66c5b954202a0259dc68d3ec17a5c236adbd0c4f
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-06-08 10:01:23 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0127'.

---
+++ tags/BL-0127/qixweb/sources/org/qixweb/util/test/TestExtendedTestCase.java
@@ -0,0 +1,142 @@
+package org.qixweb.util.test;
+
+import java.util.ArrayList;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.TestCase;
+
+import org.qixweb.util.CollectionUtil;
+
+public class TestExtendedTestCase extends TestCase
+{
+
+    public void testEmptyOnCollections() throws Exception
+    {
+        ArrayList arrayList = new ArrayList();
+        try
+        {
+            ExtendedTestCase.assertEmpty(arrayList);            
+        }
+        catch (AssertionFailedError e)
+        {
+            fail("should be empty");
+        }
+        
+        try
+        {
+            arrayList.add("an Object");
+            ExtendedTestCase.assertEmpty(arrayList);   
+            fail("should not be empty");
+        }
+        catch (AssertionFailedError e)
+        {
+           
+        }
+    }
+    
+    public void testNotEmptyOnCollections() throws Exception
+    {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("an Object");
+        try
+        {
+            ExtendedTestCase.assertNotEmpty(arrayList);            
+        }
+        catch (AssertionFailedError e)
+        {
+            fail("should not be empty");
+        }
+        
+        try
+        {
+            arrayList.remove(0);
+            ExtendedTestCase.assertNotEmpty(arrayList);   
+            fail("should be empty");
+        }
+        catch (AssertionFailedError e)
+        {
+           
+        }
+    }
+    
+    public void testNotEquals()
+    {
+        ExtendedTestCase.assertNotEquals("The objects must be different", new Integer(2), new Integer(3));
+
+        try
+        {
+            ExtendedTestCase.assertNotEquals("failure message", new Integer(2), new Integer(2));
+            fail("The objects must be equal");
+        }
+        catch (AssertionFailedError expectedFailure)
+        {
+        }
+    }
+
+    public void testFalse()
+    {
+        ExtendedTestCase.assertFalse("The object must be false", false);
+
+        try
+        {
+            ExtendedTestCase.assertFalse("message", true);
+            fail("The object must be true");
+        }
+        catch (AssertionFailedError expectedFailure)
+        {
+        }
+    }
+
+    public void testContainingString()
+    {
+        ExtendedTestCase.assert_contains("ciao should be contained in: ciao davide", "ciao davide", "ciao");
+
+        try
+        {
+            ExtendedTestCase.assert_contains("a message", "ciao davide", "pippo");
+            fail("pippo should not be contained in: ciao davide");
+        }
+        catch (AssertionFailedError expectedFailure)
+        {
+        }
+    }
+
+    public void testContainingRegex()
+    {
+        ExtendedTestCase.assert_matchesRegex("'ci.+a' should be contained in: ciao davide", "ciao davide", "ci.+a");
+        try
+        {
+            ExtendedTestCase.assert_matchesRegex("a message", "ciao\\davide", "\\d");
+            fail("No decimal should be contained in: ciao\\davide");
+        }
+        catch (AssertionFailedError expectedFailure)
+        {
+        }
+    }
+
+    public void testAssertEqualsIgnoringOrder() throws Exception
+    {
+        try
+        {
+            // ExtendedTestCase.assertEqualsIgnoringOrder("a message", CollectionUtil.listWith("primo", "secondo"), CollectionUtil.listWith("secondo", "primo"));
+        }
+        catch (AssertionFailedError expectedFailure)
+        {
+            fail("'ci.+a' should be contained in: ciao davide");
+        }
+    }
+
+    public void testAssertEqualsElementsButInDifferentOrder()
+    {
+        try
+        {
+            ExtendedTestCase.assertEqualsElementsButInDifferentOrder(CollectionUtil.listWith("primo", "secondo"), CollectionUtil.listWith("primo", "secondo"));
+            fail("Should consider the two list different");
+        }
+        catch (AssertionFailedError e)
+        {
+        }
+        ExtendedTestCase.assertEqualsElementsButInDifferentOrder(CollectionUtil.listWith("primo", "secondo"), CollectionUtil.listWith("secondo", "primo"));
+    }
+
+}
