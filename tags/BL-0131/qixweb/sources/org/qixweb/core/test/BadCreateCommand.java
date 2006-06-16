5f59478513ad5a0b0336ab81deda1c9d1f8b735d
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-06-16 14:49:20 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0131'.

---
+++ tags/BL-0131/qixweb/sources/org/qixweb/core/test/BadCreateCommand.java
@@ -0,0 +1,21 @@
+package org.qixweb.core.test;
+
+import org.qixweb.core.*;
+
+public class BadCreateCommand extends WebCommand
+{
+    public static final String FAKE_MESSAGE = "fake generated exception on create";
+
+
+    public static WebCommand create(QixwebUrl anUrl, UserData aUserData)
+    {
+        throw new RuntimeException(FAKE_MESSAGE);
+    }
+
+
+    public Browsable execute(QixwebEnvironment aEnvironment) throws Exception
+    {
+        return null;
+    }
+
+}
