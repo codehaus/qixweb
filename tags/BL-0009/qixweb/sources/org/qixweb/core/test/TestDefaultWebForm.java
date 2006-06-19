72b5c86146ed7dd41a0d941226a87c392ea868f1
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-06-19 14:49:32 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0009'.

---
+++ tags/BL-0009/qixweb/sources/org/qixweb/core/test/TestDefaultWebForm.java
@@ -0,0 +1,61 @@
+package org.qixweb.core.test;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.qixweb.core.*;
+import org.qixweb.util.test.ExtendedTestCase;
+
+public class TestDefaultWebForm extends ExtendedTestCase
+{
+
+    private static final QixwebUser ROUSER = QixwebUser.createUserWith("name", "pwd", "", "", "a@b", "cc", false, false);
+    private static final QixwebUser RWUSER = QixwebUser.createUserWith("name", "pwd", "", "", "a@b", "cc", false, true);
+
+    public static class MyDefaultWebForm extends DefaultWebForm
+    {
+        public String field1 = "val1";
+        public String field2 = "val2";
+        
+        public MyDefaultWebForm(QixwebUser user)
+        {
+            super(user);
+        }
+        protected WebAppUrl concreteActionUrl()
+        {
+            return new WebAppUrl("");
+        }
+    }
+
+    private MyDefaultWebForm itsForm;
+    
+    protected void setUp() throws Exception
+    {
+        itsForm = new MyDefaultWebForm(RWUSER);
+    }
+
+    public void testEquals()
+    {
+        assertEquals(itsForm, new MyDefaultWebForm(RWUSER));
+    }
+    
+    public void testToString()
+    {
+        assert_contains(itsForm.toString(), "val1");
+        assert_contains(itsForm.toString(), "val2");
+    }
+    
+    public void testDisabling()
+    {
+        assertTrue(itsForm.isEnabled());
+        assertTrue(itsForm.actionUrl().isEnabled());
+        itsForm.disable();
+        assertFalse(itsForm.isEnabled());
+        assertFalse(itsForm.actionUrl().isEnabled());
+    }
+    
+    public void testDisableIfReadOnlyUser()
+    {
+        assertTrue(itsForm.isEnabled());
+        itsForm = new MyDefaultWebForm(ROUSER);
+        assertFalse(itsForm.isEnabled());
+    }
+}
