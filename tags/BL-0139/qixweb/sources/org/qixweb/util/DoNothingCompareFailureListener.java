272f4c36cda1e15b7d8e70d9e01631cf31dbb764
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-06-28 10:13:32 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0139'.

---
+++ tags/BL-0139/qixweb/sources/org/qixweb/util/DoNothingCompareFailureListener.java
@@ -0,0 +1,24 @@
+package org.qixweb.util;
+
+import java.util.List;
+
+public class DoNothingCompareFailureListener implements CompareFailureListener
+{
+	public void notifyDifferentElement(Object expected, Object actual, int elementIndex)
+	{
+	}
+
+	public void notifyDifferentLength(int expectedLength, int actualLength)
+	{
+	}
+
+	public void notifyElementsNotPresent(List expected)
+	{
+	}
+
+	public String getFailureMessage()
+	{
+		return "";
+	}
+
+}
