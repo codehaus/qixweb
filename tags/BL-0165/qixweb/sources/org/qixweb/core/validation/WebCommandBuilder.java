fa948a2883c1ab85aab2f2bad7b40171c1d1ae4b
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-09-21 14:42:46 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0165'.

---
+++ tags/BL-0165/qixweb/sources/org/qixweb/core/validation/WebCommandBuilder.java
@@ -0,0 +1,54 @@
+package org.qixweb.core.validation;
+
+import java.util.HashMap;
+
+import org.qixweb.core.*;
+import org.qixweb.util.DeepEquals;
+
+public abstract class WebCommandBuilder
+{
+    public static final boolean MANDATORY = true;
+    public static final boolean OPTIONAL = !MANDATORY;
+
+    protected final Parameters itsSubmittedValues;
+    private boolean areAllRight;
+    private HashMap itsMessageForInvalidParameters;
+
+    protected WebCommandBuilder(Parameters submittedValues)
+    {
+        itsSubmittedValues = submittedValues;
+        areAllRight = true;
+        itsMessageForInvalidParameters = new HashMap();
+    }
+
+    public abstract WebCommand destinationWhenValid(QixwebUrl url, UserData userData);
+    public abstract Browsable destinationWhenNotValid(QixwebEnvironment environment);
+
+    protected void addControl(WebCommandRequestControl control, boolean mandatoryFlag, String parameterKey, String messageWhenInvalid)
+    {
+        if (mandatoryFlag == MANDATORY || itsSubmittedValues.isNotEmpty(parameterKey))
+        {
+            boolean isKeyValid = control.isValid(parameterKey);
+            if (!isKeyValid)
+                itsMessageForInvalidParameters.put(parameterKey, messageWhenInvalid);
+
+            areAllRight &= isKeyValid;
+        }
+    }
+
+    public boolean isValid()
+    {
+        return areAllRight;
+    }
+
+    public HashMap messagesForInvalidParameters()
+    {
+        return itsMessageForInvalidParameters;
+    }
+
+    public boolean equals(Object other)
+    {
+        return DeepEquals.equals(this, other);
+    }
+
+}
