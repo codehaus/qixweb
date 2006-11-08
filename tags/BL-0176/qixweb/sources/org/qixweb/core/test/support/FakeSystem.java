5f0e48f50797271779d76c6ed118acfe2e3c23d6
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-11-08 17:09:45 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0176'.

---
+++ tags/BL-0176/qixweb/sources/org/qixweb/core/test/support/FakeSystem.java
@@ -0,0 +1,8 @@
+package org.qixweb.core.test.support;
+
+import org.qixweb.core.TheSystem;
+
+public class FakeSystem implements TheSystem
+{
+	public static final String BASE_URL = "/acme/servet/";
+}
