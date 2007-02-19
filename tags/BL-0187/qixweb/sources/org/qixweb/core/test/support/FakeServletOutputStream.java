deff58da2f1ca6e83b8ce19beaa7439bd2c00a31
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2007-02-19 10:59:18 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0187'.

---
+++ tags/BL-0187/qixweb/sources/org/qixweb/core/test/support/FakeServletOutputStream.java
@@ -0,0 +1,35 @@
+package org.qixweb.core.test.support;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class FakeServletOutputStream extends javax.servlet.ServletOutputStream
+{
+	private ByteArrayOutputStream itsByteArrayStream;
+    private boolean hasBeenClosed;
+
+    public FakeServletOutputStream()
+	{
+		itsByteArrayStream = new ByteArrayOutputStream();
+        hasBeenClosed = false;
+	}
+	public void write(int aByte) throws IOException
+	{
+		itsByteArrayStream.write(aByte);
+	}
+
+	public byte[] outputAsBytes()
+	{
+		return itsByteArrayStream.toByteArray();
+	}
+    
+    public void close() throws IOException
+    {
+        hasBeenClosed = true;
+    }
+
+    public boolean hasBeenClosed()
+    {
+        return hasBeenClosed;
+    }
+}
