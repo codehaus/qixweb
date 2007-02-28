142d7f031c24dbd8b6b576349642e8ca04c07131
nobody <nobody@a9f70300-554b-0410-95b5-b18b23f37aa1>
2007-02-28 15:00:40 +0000

    This commit was manufactured by cvs2svn to create tag 'BL-0188'.

---
+++ tags/BL-0188/qixweb/sources/org/qixweb/util/ClassUtil.java
@@ -0,0 +1,51 @@
+package org.qixweb.util;
+
+import java.lang.reflect.Constructor;
+
+import org.apache.commons.lang.ClassUtils;
+import org.apache.commons.lang.StringUtils;
+
+public class ClassUtil
+{
+    private static final String INNER_CLASS_SEPARATOR = ".";
+
+    public static String fullNameOf(Class aClass)
+    {
+        return aClass.getName();
+    }
+
+    public static String shortNameOf(Class aClass)
+    {
+        String shortClassName = ClassUtils.getShortClassName(aClass);
+
+        if (ClassUtils.isInnerClass(aClass))
+            return outerClassName(shortClassName);
+        else
+            return shortClassName;
+    }
+
+    private static String outerClassName(String shortClassName)
+    {
+        return StringUtils.substringBefore(shortClassName, INNER_CLASS_SEPARATOR);
+    }
+
+    public static Object newInstance(Class clazz, Class[] classesOfArgs, Object[] args)
+    {
+        Object newInstance = null;
+        try
+        {
+            Constructor constructor = clazz.getConstructor(classesOfArgs);
+            newInstance = constructor.newInstance(args);
+        }
+        catch (Exception impossibleEx)
+        {
+            XpLogger.logException(impossibleEx);
+        }
+        return newInstance;
+    }
+
+    public static Object newInstance(Class clazz)
+    {
+        return newInstance(clazz, new Class[0], new Object[0]);
+    }
+}
